90c60aa565c77860ad31b5c208deea5d3f925942
astroed506 <93076427+astroed506@users.noreply.github.com>
2026-02-24 19:21:16 -0600

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+
+  return (
+   
+      <div>
+              <h1> Tecnology Solution</h1>
+         
+              <p>Tienda Tecnologica Online </p>
+        
+        
+      </div>
+  );
+}
+
+export default App;
